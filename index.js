7912ae73d1b45d26a7924c7a6047418792eed7d1
yuri <ymoraes5948@gmail.com>
2019-05-03 15:16:25 -0300

    modulo1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const users = [
+  'Yuri Moraes',
+  'Elaine Rodrigues',
+  'Karen Rodrigues',
+  'Hannah Rodrigues',
+  'Ana Clara Moraes'
+]
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  res.redirect('/')
+})
+
+app.listen(3000)
